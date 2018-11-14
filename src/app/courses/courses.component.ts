a3731b057c73d6de088c98b23cb1fb6cf321e441
hyjung <hyjung@wins21.com>
2018-11-14 18:25:42 +0900

    변수 수정

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  text = 'dsafasrgfgfdttfhgjhjkjkbjcghghdthdghdgcnhkhjhgdhgfdhg';
+  text = 'dsafasrgfgfdttfhgㄴㄴㄴㄴㄴjhjkjkbjcghghdthdghdgcnhkhjhgdhgfdhg';
   constructor() { }
 
   ngOnInit() {
